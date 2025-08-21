aec689747dbd7975fed189226ebeda27513e3f44
zfqdinesh <mynamedineshajmera@gmail.com>
2025-08-21 10:29:03 +0530

    new web added

--- src/pages/Contact.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Contact from '../components/Contact';
-
-const ContactPage: React.FC = () => {
-  return <Contact />;
-};
-
-export default ContactPage;
--- src/pages/Home.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Hero from '../components/Hero';
-
-const Home: React.FC = () => {
-  return <Hero />;
-};
-
-export default Home;
